7bc345746852f97712b685e27c872bff5266f1fc
Fidel Barreat <fidelbarreat16@gmail.com>
2018-03-11 14:43:24 -0500

    Deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import jackie from "./liz.jpg";
 import "./App.css";
 
 class App extends Component {
@@ -10,7 +9,6 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">¿Toda la vida jodiendo, Liz?</h1>
-          <img src={jackie} className="lizImg" alt="Liz" />
         </header>
       </div>
     );
