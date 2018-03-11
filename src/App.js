2a8a52e50ef23211804666fd2635073931438763
Fidel Barreat <fidelbarreat16@gmail.com>
2018-03-11 12:36:55 -0500

    Added Jackie Chan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import jackie from "./jackie.jpg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Liz, párame bolas</h1>
+          <img src={jackie} />
         </header>
       </div>
     );
