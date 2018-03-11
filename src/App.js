a82a7a122daa0ccbd02b4dd1ced844340bfa6d1f
Fidel Barreat <fidelbarreat16@gmail.com>
2018-03-11 14:36:23 -0500

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import jackie from "./jackie.jpg";
+import jackie from "./liz.jpg";
 import "./App.css";
 
 class App extends Component {
@@ -9,8 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Liz, párame bolas</h1>
-          <img src={jackie} />
+          <h1 className="App-title">¿Toda la vida jodiendo, Liz?</h1>
+          <img src={jackie} className="lizImg" alt="Liz" />
         </header>
       </div>
     );
